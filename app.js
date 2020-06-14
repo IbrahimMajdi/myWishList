6cbb902ebaeaf51fbb04dfa183b8ac1cd25e7280
IbrahimMajdi <ibrmaj1@gmail.com>
2020-06-14 11:50:26 +0300

    second

--- app.js
+++ app.js
@@ -1,38 +1,73 @@
 'use static'
 
 // wish constructor
+Wish.all = [];
 
+console.log("all");
 
-function Wish() {
+function Wish(name, date) {
     this.name = name;
     this.date = date;
+    Wish.all.push(this);
 }
 
-// get form for js manipulation
 
+// get form for js manipulation
 var form = document.getElementById('form');
+
+form.addEventListener("submit", handler);
+
+function handler(event) {
+    event.preventDefault();
+    
+
+
+}
+
 
 
 // store form data in the storage
-function setData(){
-    var data= document.getElementById('wishName').value;
-    localStorage.setItem('wishName',data)
-}
+function setData() {
+    var data = document.getElementById('wishName').value;
+    var date = document.getElementById('date').value;
 
+    localStorage.setItem('wishName', JSON.stringify(data))
+    localStorage.setItem('date', JSON.stringify(date))
+
+
+}
 setData();
+
+
+
+
+
+
 // get form data from the storage
-function getData(){
-    var data= document.getElementById('wishName').value;
-    localStorage.setItem('wishName',data)
+function getData() {
+
+    var retriveData = localStorage.getItem(JSON.parse('wishName'));
+    var retriveDate = localStorage.getItem(JSON.parse('date'))
+
+
+    if (Wish.all) {
+
+    }
 }
 
+// getData();
 
 
-// get table
-
-var table = document.getElementById('table')
 
 
+// get table to show render data
+
+var tbody = document.getElementById('tbody');
+
+var tdEl = document.createElement('td');
+// tbody.appendChild(tdEl);
+
+// tdEl.textContent = 
 
 
 
